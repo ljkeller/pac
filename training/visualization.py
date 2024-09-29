0bb12a04fb5ccecde35f1b04a676439c469f80a3
Lucas Keller <kellerjlucas@gmail.com>
2024-09-29 15:40:34 -0600

    Improved printing/display

--- training/visualization.py
+++ training/visualization.py
@@ -19,17 +19,17 @@ S_DB = librosa.power_to_db(S, ref=np.max)
 print(f'Shape of S: {S.shape}')
 
 fig, axs = plt.subplots(2)
-fig.suptitle('Spectrogram, Waveform')
+fig.suptitle(f'{filename} visualization')
 
 librosa.display.specshow(S_DB, sr=sample_rate, x_axis='time', y_axis='mel', ax=axs[0])
 axs[0].set_title('Mel-frequency spectrogram')
-
 fig.colorbar(mappable=axs[0].collections[0], ax=axs[0], format='%+2.0f dB', orientation='horizontal')
-
-#plt.colorbar(format='%+2.0f dB')
 
 librosa.display.waveshow(trimmed_sample, sr=sample_rate, ax=axs[1])
 axs[1].set_title('Waveform')
 axs[1].set_xlim([0, len(trimmed_sample) / sample_rate])
+
+print(f'Min, max spectrogram value: {(np.min(S_DB), np.max(S_DB))}')
+print(f'S_DB datatype: {S_DB.dtype}')
 
 plt.show()
